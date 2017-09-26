b663870cc28ad10d20a15ea3e858f10b9cb1f435
angelng1412 <ang7@stuy.edu>
2017-09-26 17:22:02 -0400

    one step forward two steps back

--- occupation.py
+++ occupation.py
@@ -12,48 +12,37 @@ def root():
 
 @app.route("/occupations")
 def occupation():
-        jobs = {}
-        createDict(reader)
-        return render_template("ace.html", table = reader)
+        return render_template("ace.html", table = reader, randjob = randJob())
 
 reader = csv.reader(open(file))
 
 
-#Job dictionary
+f = open("occupations.csv", 'r')
+s = f.read()
+data = s.split("\n")
+data = data[1:-1]
 
-
-#creates Dictionary consisting of Jobs as keys and percents as its values
-def createDict(csv):
-        for rows in csv:
-                if rows[0] == "Job Class" or rows[0] == "Total":
-                        continue
+def makeDict(): 
+        jobDict = {}
+        for job in data:
+                if job.find('"') >= 0:
+                        thing = job.split('"')
+                        jobDict[thing[1]] = float(thing[2][1:])
                 else:
-                        jobs[rows[0]] = float(rows[1])
+                        thing = job.split(',')
+                        jobDict[thing[0]] = float(thing[1])
+        return jobDict
 
-
-#pick a random job based off percentages.
-def randomJob(dict):
-        randNum = float(random.randint(1,998)/10) #create a random float
-        #print randNum
-        for keys in dict.keys():
-                #pick a random key to make it more random.
-                #  
-                #Say two jobs have the same percentage, if picking random job is not added, then the job that 
-                #       appears first will always be printed.
-                key = random.choice(dict.keys())
-                randNum = randNum - dict[key]
-                #everytime time a job is picked, subtract its percentage from randNUM
-                del dict[key]
-                #delete the job name to avoid pciking it again
-                #print key
-                if randNum <= 0:
-                        #if randNum <0, print out the job that caused this and break the loop
-                        return key
-                        break
-
-
-           
-
+def randJob():
+        dict = makeDict()
+        num = random.uniform(0,99.8)
+        tmp = 0 
+        for job, percent in dict.iteritems():
+                tmp += percent
+                if num < tmp:
+                        return job        
+                
 if __name__ == "__main__":
         app.debug = True
         app.run()
+                        
